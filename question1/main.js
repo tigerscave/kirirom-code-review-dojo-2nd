ac63fd7e2913ff6e926b32cc9acadf38d14faeb2
ChhayrithHY <hychhayrith71@gmail.com>
2019-08-30 14:18:38 +0700

    Initial commit - implemented plus, minus, and reset functionality.

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,29 @@
 "use strict"
 
 //write code in here
+const textElement = document.getElementById('number')
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+
+let total = 0
+
+function operation(operator) {
+  if (operator === "+") {
+    total++
+  } else if (operator === "-") {
+    total--
+  } else if (operator === "reset") {
+    total = 0
+  }
+
+  display(total)
+}
+
+function display(total) {
+  textElement.innerHTML = total
+}
+
+plusButton.addEventListener('click', () => operation("+"))
+minusButton.addEventListener('click', () => operation("-"))
+resetButton.addEventListener('click', () => operation("reset"))

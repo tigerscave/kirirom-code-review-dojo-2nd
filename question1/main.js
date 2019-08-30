a7b187380b03d58a40b84250b95ab6f329cda4aa
hoeurnghen <hoeurnghen17@kit.edu.kh>
2019-08-30 14:30:06 +0700

    Change var to let for numberValue

--- question1/main.js
+++ question1/main.js
@@ -5,7 +5,7 @@ const numberElement = document.getElementById('number')
 const plusButton = document.getElementById('plus')
 const minusButton = document.getElementById('minus')
 const resetButton = document.getElementById('reset')
-var numberValue = 0
+let numberValue = 0
 
 function plusNumber(){
 	numberValue += 1

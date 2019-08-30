56b8ea40037d586ac63e70adb37e06a954671d95
Sakmonysothea Youk <youksakmonysothea@gmail.com>
2019-08-30 17:35:28 +0700

    adding js code to question 3

--- question3/main.js
+++ question3/main.js
@@ -1,3 +1,43 @@
-"use strict"
+const cocacolaButton = document.getElementById('cocaCola')
+const angkorbeerButton = document.getElementById('angkorBeer')
+const moringalunchButton = document.getElementById('moringaLunch')
+const bookButton = document.getElementById('book')
+const goodsName = document.getElementById('purchaseGoodsName')
+const goodsPrice = document.getElementById('purchaseGoodsPrice')
+const payment = document.getElementById('paymentInput')
+const calculateButton = document.getElementById('calculateButton')
+const change = document.getElementById('calculatedChange')
 
-//write code in here
+let productPrice = 0 
+let pay = 0
+let changeMoney = 0
+
+function productInfo(product, price)
+{
+    productPrice = price
+    console.log(price)
+    goodsName.textContent = product
+    goodsPrice.textContent = price
+    change.textContent = ''
+    payment.value = ''
+}
+
+function calculate()
+{
+    pay = parseFloat(payment.value)
+    changeMoney = (pay - productPrice) * 4091
+    console.log(changeMoney)
+    change.textContent = Math.round(changeMoney)
+    payment.value = ''
+
+}
+
+
+
+
+cocacolaButton.addEventListener('click', () => productInfo('Coca-cola', 1.15))
+angkorbeerButton.addEventListener('click', () => productInfo('Angkor Beer', 2.5))
+moringalunchButton.addEventListener('click', () => productInfo('Moringa Lunch', 3.5))
+bookButton.addEventListener('click', () => productInfo('Book', 4.5))
+calculateButton.addEventListener('click', () => calculate())
+

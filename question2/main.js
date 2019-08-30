b5f0ff3c14f916221fcba246a2d213c7eef2768a
ChhayrithHY <hychhayrith71@gmail.com>
2019-08-30 15:14:38 +0700

    Implemented Calculate tax and Reset functionality.

--- question2/main.js
+++ question2/main.js
@@ -1,3 +1,31 @@
 "use strict"
 
 //write code in here
+const priceInput = document.getElementById('priceInput')
+const taxInput = document.getElementById('taxRatioInput')
+const taxPriceElement = document.getElementById('taxPrice')
+const taxIncludedElement = document.getElementById('taxIncludedPrice')
+const calculateButton = document.getElementById('calculateButton')
+const resetButton = document.getElementById('resetButton')
+
+
+function calculateTaxPrice(price, taxValue) {
+  let taxPrice = (price * taxValue) / 100
+  let taxIncludedPrice = Math.round(price + taxPrice)
+
+  display(Math.round(taxPrice), taxIncludedPrice, price, taxValue)
+}
+
+function resetElements() {
+  display(0, 0, '', '')
+}
+
+function display(tax, priceIncludedTax, priceInputBoxValue, taxInputBoxValue) {
+  priceInput.value = priceInputBoxValue
+  taxInput.value = taxInputBoxValue
+  taxPriceElement.innerHTML = tax
+  taxIncludedElement.innerHTML = priceIncludedTax
+}
+
+calculateButton.addEventListener('click', () => calculateTaxPrice(parseFloat(priceInput.value), parseFloat(taxInput.value)))
+resetButton.addEventListener('click', () => resetElements())

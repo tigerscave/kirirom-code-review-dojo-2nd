16ad90c88535ff263eb8ff5f453e9d146ac0598c
VUTHY ZEVO <vuthyzevo@gmail.com>
2019-08-30 17:38:48 +0700

    reduce code commit

--- question3/main.js
+++ question3/main.js
@@ -1,9 +1,3 @@
-
-// const goodsList = document.getElementById("goodsList")
-// const addGoodsButton = document.getElementById("addGoodsButton")
-// const goodsNameInput = document.getElementById("goodsNameInput")
-// const goodsPriceInput = document.getElementById("goodsPriceInput")
-
 const cocaCola = document.getElementById("cocaCola")
 const angkorBeer = document.getElementById("angkorBeer")
 const moringaLunch = document.getElementById("moringaLunch")
@@ -14,46 +8,19 @@ const calculateButton = document.getElementById("calculateButton")
 const calculatedChange = document.getElementById("calculatedChange")
 const book = document.getElementById("book")
 
-var goodsName
-var goodsPrice = cocaCola.lastElementChild.textContent
-function cocaColaClick() {
-    goodsName = cocaCola.firstElementChild.textContent
-    goodsPrice = cocaCola.lastElementChild.textContent
+function display(goodsName, goodsPrice) {
     purchaseGoodsName.textContent = goodsName
     purchaseGoodsPrice.textContent = goodsPrice.substr(1)
-
-}
-function angkorBeerClick() {
-    goodsName = angkorBeer.firstElementChild.textContent
-    goodsPrice = angkorBeer.lastElementChild.textContent
-    purchaseGoodsName.textContent = goodsName
-    purchaseGoodsPrice.textContent = goodsPrice.substr(1)
-    clearPayment()
-}
-function moringaLunchClick() {
-    goodsName = moringaLunch.firstElementChild.textContent
-    goodsPrice = moringaLunch.lastElementChild.textContent
-    purchaseGoodsName.textContent = goodsName
-    purchaseGoodsPrice.textContent = goodsPrice.substr(1)
-    clearPayment()
-}
-function bookClick() {
-    goodsName = book.firstElementChild.textContent
-    goodsPrice = book.lastElementChild.textContent
-    purchaseGoodsName.textContent = goodsName
-    purchaseGoodsPrice.textContent = goodsPrice.substr(1)
-    clearPayment()
+    paymentInput.value = ''
 }
 function calculateButtonClick() {
     let payment = parseFloat(paymentInput.value)
-    let changePrice = (payment - parseFloat(goodsPrice.substr(1))) * 4091
+    let changePrice = (payment - parseFloat(purchaseGoodsPrice.textContent.substr(1))) * 4091
     calculatedChange.textContent = changePrice
 }
-function clearPayment() {
-    paymentInput.value = ''
-}
-cocaCola.addEventListener("click", cocaColaClick)
-angkorBeer.addEventListener("click", angkorBeerClick)
-moringaLunch.addEventListener("click", moringaLunchClick)
-book.addEventListener("click", bookClick)
+
+cocaCola.addEventListener("click", ()=> display(cocaCola.firstElementChild.textContent, cocaCola.lastElementChild.textContent))
+angkorBeer.addEventListener("click", ()=> display(angkorBeer.firstElementChild.textContent, angkorBeer.lastElementChild.textContent))
+moringaLunch.addEventListener("click", ()=> display(moringaLunch.firstElementChild.textContent, moringaLunch.lastElementChild.textContent))
+book.addEventListener("click", ()=> display(book.firstElementChild.textContent, book.lastElementChild.textContent))
 calculateButton.addEventListener("click", calculateButtonClick)

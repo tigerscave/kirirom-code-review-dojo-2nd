50ee1559b28976026dab67d78b0a1b268007fcb4
Dara0505 <kruychandara17@kit.edu.kh>
2019-08-30 17:09:58 +0700

    change prase price to number before call the function

--- question3/main.js
+++ question3/main.js
@@ -4,16 +4,16 @@
 const addGoodsButton = document.getElementById('addGoodsButton')
 const goodsNameInput = document.getElementById('goodsNameInput')
 const goodsPriceInput = document.getElementById('goodsPriceInput')
-const purchaseGoodsName = document.getElementById('purchaseGoodsName')
-const purchaseGoodsPrice = document.getElementById('purchaseGoodsPrice')
+const purchaseGoodsNameEle = document.getElementById('purchaseGoodsName')
+const purchaseGoodsPriceEle = document.getElementById('purchaseGoodsPrice')
 const calculateButton = document.getElementById('calculateButton')
-const calculatedChange = document.getElementById('calculatedChange')
+const calculatedChangeEle = document.getElementById('calculatedChange')
 const goodsElements = document.getElementsByClassName('goods')
 const paymentInput = document.getElementById('paymentInput')
 
 const onGoodsClicked = (goodsName, price) => {
-    purchaseGoodsName.textContent = goodsName
-    purchaseGoodsPrice.textContent = price
+    purchaseGoodsNameEle.textContent = goodsName
+    purchaseGoodsPriceEle.textContent = price
     paymentInput.value = null
 }
 
@@ -29,8 +29,8 @@ const onCalculateBtnClicked = () => {
 
 for(let i = 0; i < goodsElements.length; i++){
     const goodsName = goodsElements[i].childNodes[1].textContent
-    const goodsPrice = goodsElements[i].childNodes[3].innerHTML.slice(1)
-    goodsElements[i].addEventListener('click', () => onGoodsClicked(goodsName, goodsPrice))
+    const price = Number(goodsElements[i].childNodes[3].innerHTML.slice(1))
+    goodsElements[i].addEventListener('click', () => onGoodsClicked(goodsName, price))
 }
 
 calculateButton.addEventListener('click', onCalculateBtnClicked)

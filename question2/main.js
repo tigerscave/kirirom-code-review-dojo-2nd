6a17174af32d03ce90d0e2b6265c0983a29b0640
HannManich <hannmanich17@kit.edu.kh>
2019-08-30 14:53:50 +0700

    update tax calculation

--- question2/main.js
+++ question2/main.js
@@ -1,3 +1,28 @@
 "use strict"
-
 //write code in here
+const calculateButton = document.getElementById("calculateButton")
+const resetButton = document.getElementById("resetButton")
+const priceInput = document.getElementById('priceInput')
+const taxRatioInput = document.getElementById('taxRatioInput')
+const taxPriceElement = document.getElementById('taxPrice')
+const taxIncludedPriceElement = document.getElementById('taxIncludedPrice')
+
+function calculateTax() {
+	if (taxRatioInput.value >= 0 && taxRatioInput.value <= 60) {
+		taxPriceElement.textContent = ((parseInt(priceInput.value) * parseInt(taxRatioInput.value)) / 100) | 0
+		taxIncludedPriceElement.textContent = parseInt(taxPriceElement.textContent) + parseInt(priceInput.value)
+	}
+	else {
+		alert("Invalid tax ratio entered")
+	}
+}
+
+function resetCalculation() {
+	taxPriceElement.textContent = "0"
+	taxIncludedPriceElement.textContent = "0"
+	priceInput.value = ""
+	taxRatioInput.value = ""
+}
+
+calculateButton.addEventListener('click', calculateTax)
+resetButton.addEventListener('click', resetCalculation)

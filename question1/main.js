da489ab67aba8747e99ac374de11698ad1b442e1
hoeurnghen <hoeurnghen17@kit.edu.kh>
2019-08-30 14:26:44 +0700

    Implement Counter By Jasmine

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,27 @@
 "use strict"
 
 //write code in here
+const numberElement = document.getElementById('number')
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+var numberValue = 0
+
+function plusNumber(){
+	numberValue += 1
+	numberElement.textContent = numberValue 
+}
+
+function minusNumber(){
+	numberValue -= 1
+	numberElement.textContent = numberValue
+}
+
+function resetNumber(){
+	numberValue = 0
+	numberElement.textContent = numberValue
+}
+
+plusButton.addEventListener('click', plusNumber)
+minusButton.addEventListener('click', minusNumber)
+resetButton.addEventListener('click', resetNumber)

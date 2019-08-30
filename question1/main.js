e9cd3c325cd817cefc06248238f3293849c02542
Thepanom Ratanawan <thepanomratanawan17@kit.edu.kh>
2019-08-30 14:04:37 +0700

    implement javaScript code in question 1

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,25 @@
 "use strict"
 
-//write code in here
+const numberElement = document.getElementById("number")
+const plusButton = document.getElementById("plus")
+const minusButton = document.getElementById("minus")
+const resetButton = document.getElementById("reset")
+
+let number = 0
+
+function calculate(operator) {
+    if(operator == '+') {
+        ++ number
+    }
+    else if(operator == '-') {
+        -- number
+    }
+    else {
+        number = 0
+    }
+    numberElement.textContent = number
+}
+
+plusButton.addEventListener('click', () => calculate('+'))
+minusButton.addEventListener('click', () => calculate('-'))
+resetButton.addEventListener('click', () => calculate('_'))

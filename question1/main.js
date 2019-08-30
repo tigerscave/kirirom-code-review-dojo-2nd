3ea7ef1a864c5023554f94293d3b8cf9d5139749
RathanakSeng <sengrathanak17@kit.edu.kh>
2019-08-30 14:05:43 +0700

    add the functionality of question-1

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,27 @@
-"use strict"
+'use strict'
 
 //write code in here
+
+let output = 0
+
+const numberElement = document.getElementById('number')
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+
+function operation(operator) {
+  if (operator == '+') {
+    output += 1
+  } else if (operator == '-') {
+    output -= 1
+  }
+  numberElement.textContent = output
+}
+function reset() {
+  output = 0
+  numberElement.textContent = output
+}
+
+plusButton.addEventListener('click', () => operation('+'))
+minusButton.addEventListener('click', () => operation('-'))
+resetButton.addEventListener('click', reset)

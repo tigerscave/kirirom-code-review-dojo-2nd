ccecb1828c7da7ef01f266e123bf0eae3d93f3b2
Dara0505 <kruychandara17@kit.edu.kh>
2019-08-30 14:49:49 +0700

    impelment calculate tax functionality

--- question2/main.js
+++ question2/main.js
@@ -3,3 +3,32 @@
 //write code in here
 const priceInput = document.getElementById('priceInput')
 const taxInput = document.getElementById('taxRatioInput')
+const calculateBtn = document.getElementById('calculateButton')
+const resetBtn = document.getElementById('resetButton')
+const taxPrice = document.getElementById('taxPrice')
+const taxIncludedPrice = document.getElementById('taxIncludedPrice')
+
+const clearInput = () => {
+    priceInput.value = null
+    taxInput.value = null
+}
+
+const onCalculateTaxBtnClicked = () => {
+    let taxInputValue =  parseInt(taxInput.value) || 0
+    let priceInputValue = parseInt(priceInput.value) || 0
+    let totalPrice = parseInt(priceInputValue + (priceInputValue * taxInputValue/100))
+    taxPrice.textContent = taxInputValue
+    taxIncludedPrice.textContent = totalPrice
+    clearInput()
+}
+
+const onBtnResetClicked = () => {
+    taxPrice.textContent = '0'
+    taxIncludedPrice.textContent = '0'
+    clearInput()
+}
+
+calculateBtn.addEventListener('click', onCalculateTaxBtnClicked)
+resetBtn.addEventListener('click', onBtnResetClicked)
+
+
--- question3/main.js
+++ question3/main.js
@@ -1,3 +1,6 @@
 "use strict"
 
 //write code in here
+const addGoodsButton = document.getElementById('addGoodsButton')
+const goodsNameInput = document.getElementById('goodsNameInput')
+const goodsPriceInput = document.getElementById('goodsPriceInput')

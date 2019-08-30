c47700dfe8273165dd7cf00c58ef33e29950c854
ChhayrithHY <hychhayrith71@gmail.com>
2019-08-30 16:43:28 +0700

    Implemented calculate change and Item click functionalities

--- question2/main.js
+++ question2/main.js
@@ -1,3 +1,37 @@
-"use strict"
+'use strict'
 
 //write code in here
+const priceInput = document.getElementById('priceInput')
+const taxInput = document.getElementById('taxRatioInput')
+const taxPriceElement = document.getElementById('taxPrice')
+const taxIncludedElement = document.getElementById('taxIncludedPrice')
+const calculateButton = document.getElementById('calculateButton')
+const resetButton = document.getElementById('resetButton')
+
+function calculateTaxPrice(price, taxValue) {
+  validateTaxRatio(taxValue)
+  const taxPrice = (price * taxValue) / 100
+  const taxIncludedPrice = Math.round(price + taxPrice)
+
+  display(Math.round(taxPrice), taxIncludedPrice, price, taxValue)
+}
+
+function resetElements() {
+  display(0, 0, '', '')
+}
+
+function validateTaxRatio(taxValue) {
+  if (taxValue < 0 || taxValue > 60) {
+    alert('Invalid tax ratio entered.')
+  }
+}
+
+function display(tax, priceIncludedTax, priceInputBoxValue, taxInputBoxValue) {
+  priceInput.value = priceInputBoxValue
+  taxInput.value = taxInputBoxValue
+  taxPriceElement.innerHTML = tax
+  taxIncludedElement.innerHTML = priceIncludedTax
+}
+
+calculateButton.addEventListener('click', () => calculateTaxPrice(parseFloat(priceInput.value), parseFloat(taxInput.value)))
+resetButton.addEventListener('click', resetElements)
--- question3/main.js
+++ question3/main.js
@@ -1,3 +1,39 @@
-"use strict"
+'use strict'
 
-//write code in here
+// Goods
+const cocaColaItem = document.getElementById('cocaCola')
+const angkorBeerItem = document.getElementById('angkorBeer')
+const moringaLunchItem = document.getElementById('moringaLunch')
+const bookServiceItem = document.getElementById('book')
+
+// Elements
+const goodsNameElement = document.getElementById('purchaseGoodsName')
+const goodsPriceElement = document.getElementById('purchaseGoodsPrice')
+const calculatedChangeElement = document.getElementById('calculatedChange')
+
+// Input Box
+const paymentInput = document.getElementById('paymentInput')
+
+// Button
+const calculateChangeButton = document.getElementById('calculateButton')
+
+const todayExchangeRate = 4091
+let selectedGoodsPrice = 0
+
+function displayProductNameAndPrice(product, price) {
+  paymentInput.value = ''
+  goodsNameElement.innerHTML = product
+  goodsPriceElement.innerHTML = price
+  selectedGoodsPrice = price
+}
+
+function calculateChange(paymentInput) {
+  const changeAmount = Math.round((paymentInput - selectedGoodsPrice) * todayExchangeRate)
+  calculatedChangeElement.innerHTML = changeAmount
+}
+
+cocaColaItem.addEventListener('click', () => displayProductNameAndPrice("Coca-cola", "1.15"))
+angkorBeerItem.addEventListener('click', () => displayProductNameAndPrice("Angkor Beer", "2.25"))
+moringaLunchItem.addEventListener('click', () => displayProductNameAndPrice("Moringa Lunch", "3.5"))
+bookServiceItem.addEventListener('click', () => displayProductNameAndPrice("Book", "4.5"))
+calculateChangeButton.addEventListener('click', () => calculateChange(parseFloat(paymentInput.value)))

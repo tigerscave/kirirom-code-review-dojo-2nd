a245a3ed8480fca472366f6fdd7941dabee2816d
dalya122899 <darodalya17@kit.edu.kh>
2019-08-30 14:16:25 +0700

    question-1/dara-dalya

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,25 @@
-"use strict"
+const count = document.getElementById('number')
+const increase = document.getElementById('plus')
+const decrease = document.getElementById('minus')
+const reset = document.getElementById('reset')
 
-//write code in here
+var num = 1
+
+function handleIncrease() {
+  num++
+  count.textContent = num
+}
+
+function handleReset(){
+  num=0
+  count.textContent = num
+}
+
+function handleDecrease(number) {
+  num--
+  count.textContent = num
+}
+
+reset.addEventListener('click', handleReset)
+increase.addEventListener('click', handleIncrease)
+decrease.addEventListener('click', handleDecrease)

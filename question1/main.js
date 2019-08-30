71d654c4abf4cbda350b7fe1b81c08329e0069b3
Sakmonysothea Youk <youksakmonysothea@gmail.com>
2019-08-30 14:23:09 +0700

    adding js code to question 1

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,32 @@
-"use strict"
+const number = document.getElementById('number')
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+var output = 0
+function doOperation(operation)
+{
+  if(operation == 'plus')
+  {
+    output += 1
+    number.textContent = output
+  }
+  else if(operation == 'minus')
+  {
+    output -= 1
+    number.textContent = output
+  }
+  else if(operation == "reset")
+  {
+    output = 0 
+    number.textContent = output
+  }
+  else
+  {
+    number.textContent = "Invalid"
+  }
+  
+}
 
-//write code in here
+plusButton.addEventListener('click', () => doOperation("plus"))
+minusButton.addEventListener('click', () => doOperation("minus"))
+resetButton.addEventListener('click', () => doOperation("reset")) 

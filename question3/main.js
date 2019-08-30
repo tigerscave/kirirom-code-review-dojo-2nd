2ccf57bc6db499068cf4138b9403e24bb7c32445
Thepanom Ratanawan <thepanomratanawan17@kit.edu.kh>
2019-08-30 16:50:20 +0700

    implement JavaScript code to question 3

--- question3/main.js
+++ question3/main.js
@@ -1,3 +1,28 @@
 "use strict"
 
-//write code in here
+const colaCola = document.getElementById("cocaCola")
+const angkorBeer = document.getElementById("angkorBeer")
+const moringaLunch = document.getElementById("moringaLunch")
+const book = document.getElementById("book")
+
+const purchaseGoodsNameDisplay = document.getElementById("purchaseGoodsName")
+const purchaseGoodsPriceDisplay = document.getElementById("purchaseGoodsPrice")
+const paymentInput = document.getElementById("paymentInput")
+const calculateButton = document.getElementById("calculateButton")
+const calculatedChange = document.getElementById("calculatedChange")
+
+function display(goodsName, goodsPrice) {
+    purchaseGoodsNameDisplay.textContent = goodsName
+    purchaseGoodsPriceDisplay.textContent = goodsPrice
+    paymentInput.value = ""
+}
+
+function calculatedChangeClick() { 
+    calculatedChange.textContent = parseInt((paymentInput.value - purchaseGoodsPriceDisplay.textContent) * 4091)
+}
+
+colaCola.addEventListener('click', () => display("Coca-Cola", 1.15))
+angkorBeer.addEventListener('click', () => display("Angkor Beer", 2.25))
+moringaLunch.addEventListener('click', () => display("Moringa Lunch", 3.5))
+book.addEventListener('click', () => display("Book", 4.5))
+calculateButton.addEventListener('click', calculatedChangeClick)

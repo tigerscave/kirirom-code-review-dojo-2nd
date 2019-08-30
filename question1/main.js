1a7736d403d6060150a89d216eb0ae802d9f44d8
Dara0505 <kruychandara17@kit.edu.kh>
2019-08-30 14:02:52 +0700

    add all funtionality to all button

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,21 @@
 "use strict"
 
 //write code in here
+let initNum = 0
+const number = document.getElementById('number')
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
+const resetBtn = document.getElementById('reset')
+
+const onBtnClicked = (operator) => () => {
+  if (operator === '+') number.textContent = (++initNum).toString()
+  else if(operator === '-') number.textContent = (--initNum).toString()
+  else {
+    initNum = 0
+    number.textContent = '0'
+  }
+}
+
+plusBtn.addEventListener('click', onBtnClicked('+'))
+minusBtn.addEventListener('click', onBtnClicked('-'))
+resetBtn.addEventListener('click', onBtnClicked(''))

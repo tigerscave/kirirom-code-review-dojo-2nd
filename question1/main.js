2951a79a947d97d0619bcf1c7f5df7102ae6cd78
songsokhavudthi <sokhavudthisong@gmail.com>
2019-08-30 14:10:02 +0700

    add function to buttons

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,27 @@
 "use strict"
 
 //write code in here
+let output = 0
+
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+const numberElement = document.getElementById('number')
+
+function calculate(operation){
+  if(operation == '+'){
+    output = output + 1
+  }
+  else if(operation == '-'){
+    output = output - 1
+  }
+  numberElement.textContent = output
+}
+function reset(){
+  output = 0
+  numberElement.textContent = output
+}
+
+plusButton.addEventListener('click', () => calculate('+'))
+minusButton.addEventListener('click', () => calculate('-'))
+resetButton.addEventListener('click', reset)

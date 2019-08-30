0b1c81016408c5794349ed9918d8312e95b3728e
VUTHY ZEVO <vuthyzevo@gmail.com>
2019-08-30 14:19:59 +0700

    fix question1/Approved commit

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,21 @@
-"use strict"
-
 //write code in here
+const plusButton = document.getElementById("plus")
+const minusButton = document.getElementById("minus")
+const resetButton = document.getElementById("reset")
+const numElement = document.getElementById("number")
+
+function plusButtonClick() {
+    number = Number(numElement.textContent) + 1
+    numElement.textContent = number
+}
+function minusButtonClick() {
+    number = Number(numElement.textContent) - 1
+    numElement.textContent = number
+}
+function resetButtonClick() {
+    numElement.textContent = 0
+}
+
+plusButton.addEventListener("click", plusButtonClick)
+minusButton.addEventListener("click", minusButtonClick)
+resetButton.addEventListener("click", resetButtonClick)

92d293dbad3891cdb5ab024d13182ffa523a8490
simchhay <simchhay17@kit.edu.kh>
2019-08-30 15:42:06 +0700

    add main.js question1

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,28 @@
 "use strict"
 
 //write code in here
+const plusButton = document.getElementById("plus")
+const minusButton = document.getElementById("minus")
+const resetButton = document.getElementById("reset")
+const numberElement = document.getElementById("number")
+
+let number = 0
+
+function onPlusButtonClick(){
+    number = number + 1
+    numberElement.textContent = number
+}
+
+function onMinusButtonClick(){
+    number = number - 1
+    numberElement.textContent = number
+}
+
+function onResetButtonClick(){
+    number = 0
+    numberElement.textContent = number
+}
+
+plusButton.addEventListener("click", onPlusButtonClick)
+minusButton.addEventListener("click", onMinusButtonClick)
+resetButton.addEventListener("click", onResetButtonClick)

d35016aecea54dfafcaab2f606c1be3af7ca78cb
chhormenghong1 <chhormenghong17@kit.edu.kh>
2019-08-30 14:10:43 +0700

    test

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,26 @@
 "use strict"
 
 //write code in here
+let num = 0
+
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+const number = document.getElementById('number')
+
+function cal(symbol){
+    if (symbol === '+'){
+        number.textContent = (++num).toString()
+    }
+    else if (symbol === '-'){
+        number.textContent = (--num).toString()
+    }
+    else {
+        number.textContent = '0'
+        num = 0
+    }
+}
+
+plusButton.addEventListener('click', () => cal('+'))
+minusButton.addEventListener('click', () => cal('-'))
+resetButton.addEventListener('click', () => cal(''))

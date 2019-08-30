e05afb6e38e1e081108b6e37440ec82b349ffe71
VUTHY ZEVO <vuthyzevo@gmail.com>
2019-08-30 14:55:36 +0700

    question-2/Approved commit

--- question2/main.js
+++ question2/main.js
@@ -1,3 +1,28 @@
-"use strict"
-
 //write code in here
+const priceInput = document.getElementById("priceInput")
+const taxRatioInput = document.getElementById("taxRatioInput")
+const calculateButton = document.getElementById("calculateButton")
+const resetButton = document.getElementById("resetButton")
+const taxPriceDisplay = document.getElementById("taxPrice")
+const taxIncludedPriceDisplay = document.getElementById("taxIncludedPrice")
+
+function calculateButtonClick(){
+    price = parseFloat(priceInput.value)
+    taxRate = parseFloat(taxRatioInput.value)
+    tax = (price * taxRate)/100
+    taxWithPrice = tax + price
+
+    taxPriceDisplay.textContent = tax
+    taxIncludedPriceDisplay.textContent = taxWithPrice
+}
+function resetButtonClick(){
+    taxPriceDisplay.textContent = 0
+    taxIncludedPriceDisplay.textContent = 0
+    clearInput()
+}
+function clearInput(){
+    priceInput.value = ''
+    taxRatioInput.value = ''
+}
+calculateButton.addEventListener("click", calculateButtonClick)
+resetButton.addEventListener("click", resetButtonClick)

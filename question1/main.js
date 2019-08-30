4bef69d4d0dced7541cf7bab0ebdce518b94f709
HannManich <hannmanich17@kit.edu.kh>
2019-08-30 14:09:23 +0700

    update simple counter

--- question1/main.js
+++ question1/main.js
@@ -1,2 +1,23 @@
 "use strict"
 //write code in here
+const plusButton = document.getElementById("plus")
+const minusButton = document.getElementById("minus")
+const resetButton = document.getElementById("reset")
+const numberElement = document.getElementById("number")
+
+function addition(){
+	numberElement.textContent++
+}
+
+function substraction(){
+	numberElement.textContent--
+}
+
+function resetNumber(){
+	numberElement.textContent = "0"
+}
+
+plusButton.addEventListener('click', addition)
+minusButton.addEventListener('click', substraction)
+resetButton.addEventListener('click', resetNumber)
+

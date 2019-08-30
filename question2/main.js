701fbad28db7678b1cc0876e90c4489d83ff5587
dalya122899 <darodalya17@kit.edu.kh>
2019-08-30 15:57:00 +0700

    implement question-2 tax calculation

--- question2/main.js
+++ question2/main.js
@@ -1,3 +1,27 @@
-"use strict"
+const priceInput = document.getElementById("priceInput")
+const taxInput = document.getElementById("taxRatioInput")
+const calculateButton = document.getElementById("calculateButton")
+const resetButton = document.getElementById("resetButton")
+const taxprice = document.getElementById("taxPrice")
+const total = document.getElementById("taxIncludedPrice")
 
-//write code in here
+
+function calculateTaxPrice(){
+	if (parseInt(taxInput.value) > 0 && parseInt(taxInput.value) <= 60){
+		taxPrice.textContent = (parseInt(taxInput.value) * parseInt(priceInput.value)) / 100
+		total.textContent = parseInt(taxPrice.textContent) + parseInt(priceInput.value)
+	}
+	else{
+		alert("Invalid tax radio entered")
+	}
+}
+
+function reset(){
+	priceInput.value = ""
+	taxInput.value = ""
+	taxPrice.textContent = 0
+	total.textContent = 0
+}
+
+calculateButton.addEventListener('click', calculateTaxPrice)
+resetButton.addEventListener('click', reset)

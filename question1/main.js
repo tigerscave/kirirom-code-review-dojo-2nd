bf9923f1b23b5131eb08a65adac778b579e191ad
TanSomnang <tansomnang17@kit.edu.kh>
2019-08-30 14:21:58 +0700

    Creation of variables and functions

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,13 @@
 "use strict"
-
-//write code in here
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+const textElement = document.getElementById('number')
+var count = 0
+function changeNum(number)
+{
+  textElement.textContent=number
+}
+plusButton.addEventListener('click', () => changeNum(count += 1))
+minusButton.addEventListener('click', () => changeNum(--count))
+resetButton.addEventListener('click', () => changeNum(count = 0))

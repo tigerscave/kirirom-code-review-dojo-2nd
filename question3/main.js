bc03ce459126fce4d33b39912671206beca57410
Dara0505 <kruychandara17@kit.edu.kh>
2019-08-30 16:40:26 +0700

    implement calculate price functionality

--- question3/main.js
+++ question3/main.js
@@ -8,3 +8,27 @@ const purchaseGoodsName = document.getElementById('purchaseGoodsName')
 const purchaseGoodsPrice = document.getElementById('purchaseGoodsPrice')
 const calculateButton = document.getElementById('calculateButton')
 const calculatedChange = document.getElementById('calculatedChange')
+const goodsElements = document.getElementsByClassName('goods')
+const paymentInput = document.getElementById('paymentInput')
+
+const onGoodsClicked = (goodsName, price) => {
+    purchaseGoodsName.textContent = goodsName
+    purchaseGoodsPrice.textContent = price
+    paymentInput.value = null
+}
+
+const onCalculateBtnClicked = () => {
+    let paymentVal = paymentInput.value
+    if(isNaN(paymentVal) || paymentVal === '') return
+    let result = Math.floor((paymentVal -  Number(purchaseGoodsPrice.textContent)) * 4091)
+    calculatedChange.innerHTML = result
+    paymentInput.value = null
+}
+
+for(let i = 0; i < goodsElements.length; i++){
+    let goodsName = goodsElements[i].childNodes[1].textContent
+    let goodsPrice = goodsElements[i].childNodes[3].innerHTML.slice(1)
+    goodsElements[i].addEventListener('click', () => onGoodsClicked(goodsName, goodsPrice))
+}
+
+calculateButton.addEventListener('click', onCalculateBtnClicked)

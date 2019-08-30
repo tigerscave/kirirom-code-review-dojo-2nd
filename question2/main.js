29e4beab0d2d0cca55bb0c83e343d91a9e4946a8
MYSELF <sunchenny17@kit.edu.kh>
2019-08-30 14:55:24 +0700

    Compelete Question 2

--- question2/main.js
+++ question2/main.js
@@ -1,3 +1,30 @@
 "use strict"
 
 //write code in here
+
+const priceInput = document.getElementById ("priceInput")
+const taxInput = document.getElementById ("taxRatioInput")
+
+const calculateButton = document.getElementById ("calculateButton")
+const resetButton = document.getElementById ("resetButton")
+
+const taxPrice = document.getElementById("taxPrice")
+const taxIncludedPrice = document.getElementById("taxIncludedPrice")
+
+calculateButton.addEventListener('click', () => {
+    let price = parseInt(priceInput.value)
+    let taxRatio = parseInt(taxInput.value)
+
+    let taxPriceResult = (price * taxRatio) / 100
+
+    taxPrice.textContent = taxPriceResult
+    taxIncludedPrice.textContent = price + taxPriceResult
+
+    priceInput.value = null
+    taxInput.value = null
+})
+
+resetButton.addEventListener('click', () => {
+    taxPrice.textContent = 0
+    taxIncludedPrice.textContent = 0
+})

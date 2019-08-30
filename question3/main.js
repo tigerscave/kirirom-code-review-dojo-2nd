0d434de63c62179951204c304176b1ab926ed23d
Dara0505 <kruychandara17@kit.edu.kh>
2019-08-30 14:56:45 +0700

    declare dom

--- question3/main.js
+++ question3/main.js
@@ -4,3 +4,7 @@
 const addGoodsButton = document.getElementById('addGoodsButton')
 const goodsNameInput = document.getElementById('goodsNameInput')
 const goodsPriceInput = document.getElementById('goodsPriceInput')
+const purchaseGoodsName = document.getElementById('purchaseGoodsName')
+const purchaseGoodsPrice = document.getElementById('purchaseGoodsPrice')
+const calculateButton = document.getElementById('calculateButton')
+const calculatedChange = document.getElementById('calculatedChange')

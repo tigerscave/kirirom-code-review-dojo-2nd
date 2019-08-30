96f4cabe936bec034b9b3fd98eddce7890be7fb4
SangSonyratt <34526926+SangSonyratt@users.noreply.github.com>
2019-08-30 16:47:56 +0700

    add functionalities for price calculation

--- question3/main.js
+++ question3/main.js
@@ -1,3 +1,38 @@
 "use strict"
 
 //write code in here
+const purchaseGoodsName = document.getElementById('purchaseGoodsName')
+const purchaseGoodsPrice= document.getElementById('purchaseGoodsPrice')
+const calculateButton = document.getElementById('calculateButton')
+const calculatedChange = document.getElementById('calculatedChange')
+const paymentInput = document.getElementById('paymentInput')
+const goodsList = document.getElementsByClassName('goods')
+let khrExchangeRate = 4091
+let clickedGoods = ''
+let goodsPrice = 0
+
+function onGoodsClick(goods) {
+    let goodsId = getGoodsId(goods)
+    clickedGoods = document.getElementById(goodsId)
+    goodsPrice = clickedGoods.children[1].textContent
+    goodsPrice = parseFloat(goodsPrice.slice(1,goodsPrice.length))
+    purchaseGoodsName.textContent = clickedGoods.children[0].textContent
+    purchaseGoodsPrice.textContent = goodsPrice
+    paymentInput.value = ''
+    calculatedChange.textContent = ''
+}
+
+function onCalculateChangeClick() {
+    let change = parseFloat(paymentInput.value) - goodsPrice
+    calculatedChange.textContent = Math.round(change * khrExchangeRate)
+    paymentInput.value = ''
+}
+
+function getGoodsId(goods) {
+    return goods.id
+}
+
+calculateButton.addEventListener('click', onCalculateChangeClick)
+for(let goods of goodsList) {
+    goods.addEventListener('click', () => onGoodsClick(goods))
+}

ca22f82363f955c939e3b42078da0e691d8f2c8a
moonlight <miratorimoonlight@gmail.com>
2019-08-30 14:05:38 +0700

    Initial commit - add functionality to buttons

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,33 @@
-"use strict"
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')
+var displayNum = document.getElementById('number')
 
-//write code in here
+plusButton.addEventListener('click', ()=>clickButton('plus'))
+minusButton.addEventListener('click', ()=>clickButton('minus'))
+resetButton.addEventListener('click', ()=>clickButton('reset'))
+
+let num = 0
+function clickButton(buttonName)
+{
+    if(buttonName == 'plus')
+    {
+        num = num + 1
+        displayNum.textContent = num
+    }    
+    else if(buttonName == 'minus')
+    {
+        num = num - 1
+        displayNum.textContent = num
+    }    
+    else
+    {
+        num = 0
+        displayNum.textContent = num
+    }
+}
+
+
+
+
+

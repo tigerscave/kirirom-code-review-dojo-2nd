a3c129e5525ee0003359fa5cdb9cfd6aa9db9bb3
Sakmonysothea Youk <youksakmonysothea@gmail.com>
2019-08-30 15:20:06 +0700

    adding js code to question 2

--- question2/main.js
+++ question2/main.js
@@ -1,3 +1,32 @@
-"use strict"
+const price = document.getElementById('priceInput')
+const taxRatio = document.getElementById('taxRatioInput')
+const resetButton = document.getElementById('resetButton')
+const CalculateButton = document.getElementById('calculateButton')
+const taxOutput = document.getElementById('taxPrice')
+const totalPrice = document.getElementById('taxIncludedPrice')
 
-//write code in here
+function calculateTax(action)
+{
+    let tax = 0
+    let taxIncludedPrice = 0
+	if(action == 'calculate')
+	{
+		taxIncludedPrice = parseInt(parseInt(price.value) + parseInt(taxRatio.value) * parseInt(price.value) / 100)
+        tax = parseInt(parseInt(taxRatio.value) * parseInt(price.value) / 100)
+        totalPrice.textContent = taxIncludedPrice
+        taxOutput.textContent = tax
+	}
+	else if (action == 'reset')
+	{
+        tax = 0
+        taxIncludedPrice = 0
+        taxOutput.textContent = tax
+        totalPrice.textContent = taxIncludedPrice
+        document.getElementById('priceInput').value = "" 
+        document.getElementById('taxRatioInput').value = ""
+        // price.placeholder = "price"
+        // taxRatio.placeholder = "tax(%)"
+    }
+}
+CalculateButton.addEventListener('click', () => calculateTax("calculate"))
+resetButton.addEventListener('click', () => calculateTax("reset")) 

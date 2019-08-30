80e9faf7c853743c4e705170011bf36999370809
SangSonyratt <34526926+SangSonyratt@users.noreply.github.com>
2019-08-30 14:14:14 +0700

    Add functionalities

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,25 @@
 "use strict"
 
-//write code in here
+const minusButton = document.getElementById('minus')
+const plusButton = document.getElementById('plus')
+const number = document.getElementById('number')
+const resetButton = document.getElementById('reset')
+
+function minusButtonClick()
+{
+    number.textContent = Number(number.textContent) - 1
+}
+
+function plusButtonClick()
+{
+    number.textContent = Number(number.textContent) + 1
+}
+
+function resetButtonClick()
+{
+    number.textContent = 0
+}
+
+minusButton.addEventListener('click', minusButtonClick)
+plusButton.addEventListener('click', plusButtonClick)
+resetButton.addEventListener('click', resetButtonClick)

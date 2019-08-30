129636e5a81e9bd54b17e5b05a0d5ac0f4b75ce1
Vansen Hengmeanrith <chris_legaxy@yahoo.com>
2019-08-30 17:14:44 +0700

    Complete Question 3

--- question2/main.js
+++ question2/main.js
@@ -1,3 +1,31 @@
 "use strict"
 
 //write code in here
+const priceInput = document.getElementById('priceInput')
+const taxRatioInput = document.getElementById('taxRatioInput')
+
+const taxPrice = document.getElementById('taxPrice')
+const taxIncludedPrice = document.getElementById('taxIncludedPrice')
+
+const calculateButton = document.getElementById('calculateButton')
+const resetButton = document.getElementById('resetButton')
+
+calculateButton.addEventListener('click', () => {
+    var price = priceInput.value
+    var tax = taxRatioInput.value
+
+    const taxPriceCal = (price * tax) / 100
+
+    price = parseInt(price) + parseInt(taxPriceCal)
+
+    taxPrice.textContent = taxPriceCal
+    taxIncludedPrice.textContent = price
+
+    priceInput.value = null
+    taxRatioInput.value = null
+})
+
+resetButton.addEventListener('click', () => {
+    taxPrice.textContent = 0
+    taxIncludedPrice.textContent = 0
+})
--- question3/main.js
+++ question3/main.js
@@ -1,3 +1,65 @@
 "use strict"
 
 //write code in here
+
+const cocaColaButton = document.getElementById('cocaCola')
+const angkorBeerButton = document.getElementById('angkorBeer')
+const moringaLunchButton = document.getElementById('moringaLunch')
+const bookButton = document.getElementById('book')
+const calculateButton = document.getElementById('calculateButton')
+
+const purchaseGoodsName = document.getElementById('purchaseGoodsName')
+const purchaseGoodsPrice = document.getElementById('purchaseGoodsPrice')
+
+const paymentInput = document.getElementById('paymentInput')
+const calculatedChange = document.getElementById('calculatedChange')
+
+let selectedGoodItem = ""
+
+function separateContent(good) {
+    const goodChildren = good.children
+    
+    const goodName = goodChildren[0].textContent
+    const goodPriceWithDollarSign = goodChildren[1].textContent
+
+    const goodPriceSeparate = goodPriceWithDollarSign.split('$')
+    const goodPrice = goodPriceSeparate[1]
+
+    const goodDetail = [goodName, goodPrice]
+
+    return goodDetail
+}
+
+function payment(paymentInput) {
+    const goodDetail = separateContent(selectedGoodItem)
+
+    const goodPrice = goodDetail[1]
+    const paymentInDollar = paymentInput.value
+
+    const paymentResult = paymentInDollar - goodPrice
+
+    const changeInRiels = Math.floor(paymentResult*4091)
+
+    if(changeInRiels < 0) {
+        alert("Please enter a higher amount")
+    } else {
+        calculatedChange.textContent = changeInRiels
+    }
+}
+
+function selectedGood(good) {
+    const goodDetail = separateContent(good)
+    const goodName = goodDetail[0]
+    const goodPrice = goodDetail[1]
+
+    purchaseGoodsName.textContent = goodName
+    purchaseGoodsPrice.textContent = goodPrice
+
+    selectedGoodItem = good
+}
+
+cocaColaButton.addEventListener('click', () => selectedGood(cocaColaButton))
+angkorBeerButton.addEventListener('click', () => selectedGood(angkorBeerButton))
+moringaLunchButton.addEventListener('click', () => selectedGood(moringaLunchButton))
+bookButton.addEventListener('click', () => selectedGood(bookButton))
+calculateButton.addEventListener('click', () => payment(paymentInput))

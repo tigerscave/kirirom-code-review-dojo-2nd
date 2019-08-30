c6a986483c52ff0ce668b99e970bfcbc5530b412
chhormenghong1 <chhormenghong17@kit.edu.kh>
2019-08-30 15:42:37 +0700

    tax calculation

--- question2/main.js
+++ question2/main.js
@@ -1,3 +1,29 @@
 "use strict"
 
 //write code in here
+const priceInput = document.getElementById('priceInput')
+const taxRatioInput = document.getElementById('taxRatioInput')
+const calButton = document.getElementById('calculateButton')
+const resetButton = document.getElementById('resetButton')
+const taxPrice = document.getElementById('taxPrice')
+const taxInclude = document.getElementById('taxIncludedPrice')
+
+const taxCalculation = text => () => {
+    const price = parseInt(priceInput.value)
+    const taxRat = parseInt(taxRatioInput.value)
+    if (text === 'cal') {
+        taxPrice.textContent = Math.floor((price*taxRat)/100);
+        taxInclude.textContent = price + parseInt(taxPrice.textContent);
+    } 
+}
+
+function onResetBtnClicked()
+{
+    taxPrice.textContent = "0";
+    taxInclude.textContent = "0";
+    priceInput.value = '';
+    taxRatioInput.value= '';
+}
+
+calButton.addEventListener('click', taxCalculation('cal'))
+resetButton.addEventListener('click', onResetBtnClicked)

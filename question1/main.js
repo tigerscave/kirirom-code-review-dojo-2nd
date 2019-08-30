07bce9b1b7dd928047740bc12efdbd3675ac11e2
MYSELF <sunchenny17@kit.edu.kh>
2019-08-30 14:08:49 +0700

    complete question 1

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,20 @@
-"use strict"
+const plusButton = document.getElementById("plus")
+const minusButton = document.getElementById("minus")
+const resetButton = document.getElementById("reset")
 
-//write code in here
+const numberElement = document.getElementById("number")
+
+let number = parseInt(numberElement.textContent)
+
+plusButton.addEventListener('click', () => {
+    number += 1
+    numberElement.textContent = number 
+})
+minusButton.addEventListener('click', () => {
+    number -= 1
+    numberElement.textContent = number 
+})
+resetButton.addEventListener('click', () => {
+    number = 0
+    numberElement.textContent = number 
+})

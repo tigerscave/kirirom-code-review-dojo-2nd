a3b047d1dafa80cd20c8b2025f34febd2d6a3931
RathanakSeng <sengrathanak17@kit.edu.kh>
2019-08-30 14:54:10 +0700

    add the functionality of question-2

--- question2/main.js
+++ question2/main.js
@@ -1,3 +1,35 @@
-"use strict"
+'use strict'
 
 //write code in here
+let tax = 0
+let taxIncludedPrice = 0
+
+const taxPriceElement = document.getElementById('taxPrice')
+const taxIncludedPriceElement = document.getElementById('taxIncludedPrice')
+const priceInput = document.getElementById('priceInput')
+const taxRatioInput = document.getElementById('taxRatioInput')
+const calculateButton = document.getElementById('calculateButton')
+const resetButton = document.getElementById('resetButton')
+
+function calculate() {
+  tax = parseInt(priceInput.value) * (parseInt(taxRatioInput.value) / 100)
+  tax = Math.round(parseInt(tax))
+  taxIncludedPrice = parseInt(priceInput.value) + tax
+
+  taxPriceElement.textContent = tax
+  taxIncludedPriceElement.textContent = taxIncludedPrice
+  priceInput.value = ''
+  taxRatioInput.value = ''
+}
+function reset() {
+  tax = 0
+  taxIncludedPrice = 0
+
+  taxPriceElement.textContent = tax
+  taxIncludedPriceElement.textContent = taxIncludedPrice
+  priceInput.value = ''
+  taxRatioInput.value = ''
+}
+
+calculateButton.addEventListener('click', calculate)
+resetButton.addEventListener('click', reset)

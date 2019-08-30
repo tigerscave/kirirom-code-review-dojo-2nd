89aa456bb5a846f32bc5fb0b86287fc065312bef
Sakmonysothea Youk <youksakmonysothea@gmail.com>
2019-08-30 16:03:07 +0700

    added alert message for invalid inputs

--- question2/main.js
+++ question2/main.js
@@ -9,23 +9,32 @@ function calculateTax(action)
 {
     let tax = 0
     let taxIncludedPrice = 0
-	if(action == 'calculate')
-	{
-		taxIncludedPrice = parseInt(parseInt(price.value) + parseInt(taxRatio.value) * parseInt(price.value) / 100)
-        tax = parseInt(parseInt(taxRatio.value) * parseInt(price.value) / 100)
-        totalPrice.textContent = taxIncludedPrice
-        taxOutput.textContent = tax
-	}
-	else if (action == 'reset')
-	{
-        tax = 0
-        taxIncludedPrice = 0
-        taxOutput.textContent = tax
-        totalPrice.textContent = taxIncludedPrice
-        document.getElementById('priceInput').value = "" 
-        document.getElementById('taxRatioInput').value = ""
-        // price.placeholder = "price"
-        // taxRatio.placeholder = "tax(%)"
+
+    if(taxRatio.value >= 60 || taxRatio.value <= 0 || isNaN(taxRatio.value + price.value))
+    {
+        alert("Invalid tax!")
+    }
+    
+    else 
+    {
+        let priceValue = parseInt(price.value)
+        let taxValue = parseInt(taxRatio.value)
+        if(action == 'calculate')
+        {
+            taxIncludedPrice = parseInt(priceValue + taxValue * priceValue / 100)
+            tax = parseInt(taxValue * priceValue / 100)
+            totalPrice.textContent = taxIncludedPrice
+            taxOutput.textContent = tax
+        }
+        else if (action == 'reset')
+        {
+            tax = 0
+            taxIncludedPrice = 0
+            taxOutput.textContent = tax
+            totalPrice.textContent = taxIncludedPrice
+            document.getElementById('priceInput').value = "" 
+            document.getElementById('taxRatioInput').value = ""
+        }
     }
 }
 CalculateButton.addEventListener('click', () => calculateTax("calculate"))

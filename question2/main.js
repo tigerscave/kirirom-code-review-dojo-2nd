c8e4450cc2c56ba20eb30f701f77037db2703bff
chheanpisethpanha <chheanpisethpanha17@kit.edu.kh>
2019-08-30 15:50:42 +0700

    solve tax calculation

--- question2/main.js
+++ question2/main.js
@@ -1,4 +1,4 @@
-"use strict"
+'use strict'
 
 //write code in here
 const priceInput = document.getElementById('priceInput')
@@ -9,25 +9,27 @@ const resetButton = document.getElementById('resetButton')
 const taxPrice = document.getElementById('taxPrice')
 const taxIncludedPrice = document.getElementById('taxIncludedPrice')
 
-let tax = 0
-let taxIncluded = 0
-
 function calculateTax() {
     const inputPrice = parseInt(priceInput.value)
     const taxRatio = parseInt(taxRatioInput.value)
-    tax = Math.floor((inputPrice * taxRatio)/100)
-    taxIncluded = inputPrice + tax
-    taxPrice.textContent = (tax).toString()
-    taxIncludedPrice.textContent = (taxIncluded).toString()
+    if(!validTax(taxRatio)) return  
+    let tax = Math.floor((inputPrice * taxRatio) / 100)
+    let taxIncluded = inputPrice + tax
+    taxPrice.textContent = tax
+    taxIncludedPrice.textContent = taxIncluded
 }
 
 function reset() {
-    taxPrice.textContent = '0'
-    taxIncludedPrice.textContent = '0'
+    taxPrice.textContent = 0
+    taxIncludedPrice.textContent = 0
     priceInput.value = ''
     taxRatioInput.value = ''
 }
-
+function validTax(taxRatio){
+    if(taxRatio<0 || taxRatio>60)
+    alert('Invalid tax ratio entered')
+    return fasle
+}
 
 calculateButton.addEventListener('click', calculateTax)
 resetButton.addEventListener('click', reset)

44bd2996c7bb10aea547ad09f1587d465afd2918
chheanpisethpanha <chheanpisethpanha17@kit.edu.kh>
2019-08-30 14:08:32 +0700

    solve simple counter

--- question1/main.js
+++ question1/main.js
@@ -1,3 +1,26 @@
 "use strict"
 
 //write code in here
+const count = document.getElementById('number')
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const resetButton = document.getElementById('reset')  
+var num = 0
+const numberElement = document.getElementById('number')
+
+function increase() {
+    num++
+    count.textContent = num
+}
+function decrease() {
+    num--
+    count.textContent = num
+}
+function reset() {
+    num=0
+    count.textContent = num
+}
+
+plusButton.addEventListener('click', () => increase())
+minusButton.addEventListener('click',() => decrease())
+resetButton.addEventListener('click', () => reset())

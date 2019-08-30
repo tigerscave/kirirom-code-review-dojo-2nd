acfcb5120df59a3130d17e8d66ad92607288b499
chhormenghong1 <chhormenghong17@kit.edu.kh>
2019-08-30 16:54:49 +0700

    dollar-riel change

--- question3/main.js
+++ question3/main.js
@@ -1,3 +1,29 @@
 "use strict"
 
 //write code in here
+
+const cocaColaBtn = document.getElementById('cocaCola')
+const angkorBtn = document.getElementById('angkorBeer')
+const moringaLunchBtn = document.getElementById('moringaLunch')
+const purchaseGoodsPrice = document.getElementById('purchaseGoodsPrice')
+const bookBtn = document.getElementById('book')
+const input = document.getElementById('paymentInput')
+const purchaseName = document.getElementById('purchaseGoodsName')
+const calculatedChange = document.getElementById('calculatedChange')
+function priceShow (goods,price) 
+{
+    purchaseName.textContent = goods
+    purchaseGoodsPrice.textContent = price   
+}
+
+
+function calculate()
+{
+    let val = parseInt(input.val)
+    calculatedChange.textContent = 
+}
+
+cocaColaBtn.addEventListener('click',()=>priceShow('cocaCola', '1.15'))
+angkorBtn.addEventListener('click',()=>priceShow('angkorBeer', '2.25'))
+moringaLunchBtn.addEventListener('click',()=>priceShow('moringaLunch','3.5'))
+bookBtn.addEventListener('click',()=>priceShow('book','4.5'))
